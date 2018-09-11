8a87aa64a5d89505d6356e9023ac64ca91576ab6
Lauren Santosuosso <Lauren.Santosuosso@yahoo.com>
2018-09-11 19:03:57 -0400

    added firebase stuff to js

---
+++ assets/javascript/EDM.js
@@ -0,0 +1,13 @@
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDSwWBY5mAclqHG1OOd_HbCuVULUl7fvmQ",
+    authDomain: "edmfirebase.firebaseapp.com",
+    databaseURL: "https://edmfirebase.firebaseio.com",
+    projectId: "edmfirebase",
+    storageBucket: "",
+    messagingSenderId: "383403652935"
+  };
+  firebase.initializeApp(config);
